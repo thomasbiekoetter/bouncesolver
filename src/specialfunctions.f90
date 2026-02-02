30a00c214c919d909ea268941e25f43a624930ff
Thomas <thomas.biekoetter@desy.de>
2026-02-02 15:43:22 +0100

    Added module for maths functions. Added modified Bessel function of the first kind. Checked against scipy.special.iv and fullerton_bessel.

---
+++ src/specialfunctions.f90
@@ -0,0 +1,56 @@
+module bouncesolver__specialfunction
+
+  use bouncesolver__config, only : wp
+
+  implicit none
+
+  private
+
+  public :: bessel_mod_1
+
+  real(wp), parameter :: p1 = 0.5e0_wp
+  real(wp), parameter :: p2 = 0.87890594e0_wp
+  real(wp), parameter :: p3 = 0.51498869e0_wp
+  real(wp), parameter :: p4 = 0.15084934e0_wp
+  real(wp), parameter :: p5 = 0.2658733e-1_wp
+  real(wp), parameter :: p6 = 0.301532e-2_wp
+  real(wp), parameter :: p7 = 0.32411e-3_wp
+
+  real(wp), parameter :: q1 = 0.39894228e0_wp
+  real(wp), parameter :: q2 = -0.3988024e-1_wp
+  real(wp), parameter :: q3 = -0.362018e-2_wp
+  real(wp), parameter :: q4 = 0.163801e-2_wp
+  real(wp), parameter :: q5 = -0.1031555e-1_wp
+  real(wp), parameter :: q6 = 0.2282967e-1_wp
+  real(wp), parameter :: q7 = -0.2895312e-1_wp
+  real(wp), parameter :: q8 = 0.1787654e-1_wp
+  real(wp), parameter :: q9 = -0.420059e-2_wp
+
+contains
+
+  ! Source: NUMERICAL RECIPES IN FORTRAN 77
+  pure function bessel_mod_1(x) result(res)
+
+    real(wp), intent(in) :: x
+    real(wp) :: res
+
+    real(wp) :: ax
+    real(wp) :: y
+
+    if (abs(x) < 3.75e0_wp) then
+      y = (x / 3.75e0_wp) ** 2
+      res = x * (p1 + y * (p2 + y * (p3 +  &
+        y * (p4 + y * (p5 + y * (p6 + y * p7))))))
+    else
+      ax = abs(x)
+      y = 3.75e0_wp / ax
+      res = (exp(ax) / sqrt(ax)) * (q1 +  &
+        y * (q2 + y * (q3 + y * (q4 +  &
+        y * (q5 + y * (q6 + y * (q7 + &
+        y * (q8 + y * q9))))))))
+      if (x < 0.0e0_wp) res = -res
+    endif
+
+  end function bessel_mod_1
+
+end module bouncesolver__specialfunction
